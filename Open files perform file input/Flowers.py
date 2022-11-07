dd4632ffa28f6d2bb0c28400f00b2dedc93f3142
Towskister <Towskister@gmail.com>
2022-11-06 21:41:50 -0500

    update new project

---
+++ Open files perform file input/Flowers.py
@@ -0,0 +1,10 @@
+# Flowers.py - This program reads names of flowers and whether they are grown in shade or sun from an input
+# file and prints the information to the user's screen.
+# Input:  flowers.dat.
+# Output: Names of flowers and the words sun or shade.
+
+# Open input file
+
+# Write while loop here
+    # Print flower name using the following format
+    # print(var + " grows in the " + var2)
