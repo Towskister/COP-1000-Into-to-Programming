7ce2501230ad7d13d3d3f1630afa53c2a7775739
Towskister <Towskister@gmail.com>
2022-11-27 20:38:27 -0500

    update funcwnp files

---
+++ Functions with No Parameters/ArtShow.py
@@ -0,0 +1,16 @@
+"""
+ArtShow.py - This program determines if an art show attendee gets a
+                5% discount for preregistering.
+Input: Interactive.
+Output: A statement telling the user if they get a discount or no discount.
+"""
+
+# Write discount function here.
+
+
+# Write noDiscount function here.
+
+if __name__ == '__main__':
+    registerString = input("Did you preregister? Enter Y or N: ")
+
+    # Test input here. If Y, call discount(), else call noDiscount().
--- Using Multidimensional Lists/DayCare.py
+++ Using Multidimensional Lists/DayCare.py
@@ -1,20 +1,22 @@
 # Declare two-dimensional list here
-dailyRates = [[30.00, 60.00, 88.00, 115.00, 140.00], [26.00, 52.00, 70.00, 96.00, 120.00], [24.00, 46.00, 67.00, 89.00, 110.00], [22.00, 40.00, 60.00	75.00	88.00 ]]
+dailyRates = [
+    [30.00, 60.00, 88.00, 115.00, 140.00],
+    [26.00, 52.00, 70.00, 96.00, 120.00],
+    [24.00, 46.00, 67.00, 89.00, 110.00],
+    [22.00, 40.00, 60.00, 75.00, 88.00],
+    [20.00, 35.00, 50.00, 66.00, 84.00]
+]
 # Declare other variables
 QUIT = 99
-
+# Perform a priming read to get the age of the child
 age = int(input("Enter the age of the child or 99 to quit: "))
 
-# Perform a priming read to get the age of the child
 while age != QUIT:
-
     # Ask the user to enter the number of days
-
+    days = int(input("Enter the number of days: "))
     # Print the weekly rate
-
+    days -= 1
+    print(dailyRates[age][days])
     # Ask the user to enter the next child's age
-
+    age = int(input("Enter the age of the child or 99 to quit: "))
 print("End of program")
-
-
-print(dailyRates[0][0]) # how to output ML, still need to flesh out table
