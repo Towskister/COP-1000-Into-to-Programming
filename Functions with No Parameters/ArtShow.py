a5ab3d1c443745e837b48a0d1a17b20eaaa2f2a5
Towskister <Towskister@gmail.com>
2022-11-27 20:59:33 -0500

    update funcwnp files, finished assnmnt

--- Functions with No Parameters/ArtShow.py
+++ Functions with No Parameters/ArtShow.py
@@ -4,13 +4,24 @@ ArtShow.py - This program determines if an art show attendee gets a
 Input: Interactive.
 Output: A statement telling the user if they get a discount or no discount.
 """
+registerString = "Y"
+
 
 # Write discount function here.
+def discount():
+    print("You are pre-registered and qualify for a 5% discount.")
 
 
 # Write noDiscount function here.
+def noDiscount():
+    print("Sorry, you did not pre-register and do not qualify for a 5% discount.")
+
 
 if __name__ == '__main__':
     registerString = input("Did you preregister? Enter Y or N: ")
 
     # Test input here. If Y, call discount(), else call noDiscount().
+if registerString == "Y":
+    discount()
+else:
+    noDiscount()
