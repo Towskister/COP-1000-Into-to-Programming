6f8af1b06c1dcd02d058b09797213c25f44540ba
Towskister <Towskister@gmail.com>
2022-11-29 00:46:56 -0500

    finished pass list to func in py

--- Passing Lists to Functions in Python/Reverse.py
+++ Passing Lists to Functions in Python/Reverse.py
@@ -6,12 +6,18 @@ Output: Original contents of array and the reversed contents of the array.
 
 
 # Write reverseArray function here.
+def reverseArray(a):
+    numlist = list(a)
+    numlist = list(reversed(numlist))
+    return numlist
 
 
 numbers = [9, 8, 7, 6, 5]
 
 # Print contents of array.
-
+print("Original contents of array:", numbers)
 # Call reverseArray function here.
-
+rnumbers = reverseArray(numbers)
 # Print contents of reversed array.
+print("Reversed contents of array:", rnumbers)
+
