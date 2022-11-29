a7d8ac34976ec1a9f8fd0ab93dc3cba5765ce41e
Towskister <Towskister@gmail.com>
2022-11-28 23:31:16 -0500

    added pass list to func in py

---
+++ Passing Lists to Functions in Python/Reverse.py
@@ -0,0 +1,17 @@
+"""
+Reverse.py - This program reverses numbers stored in an array.
+Input: Interactive.
+Output: Original contents of array and the reversed contents of the array.
+"""
+
+
+# Write reverseArray function here.
+
+
+numbers = [9, 8, 7, 6, 5]
+
+# Print contents of array.
+
+# Call reverseArray function here.
+
+# Print contents of reversed array.
