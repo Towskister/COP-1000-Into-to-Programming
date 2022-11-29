70f70618b3034540ef2786786020c7a9c18351f9
Towskister <Towskister@gmail.com>
2022-11-28 22:47:53 -0500

    finished wrt func mult params

--- Writing Functions that Require Multiple Parameters in Python/Computation.py
+++ Writing Functions that Require Multiple Parameters in Python/Computation.py
@@ -2,17 +2,27 @@
 # Input: Interactive
 # Output: Sum, difference, and product of two values.
 
-# Write calculateSum function here
+def calculateSum(x, y):  # Write calculateSum function here
+    a = x + y
+    print(a)
 
-# Write calculateDifference function here
 
-# Write calculateProduct function here
+def calculateDifference(x, y):  # Write calculateDifference function here
+    a = x - y
+    print(a)
+
+
+def calculateProduct(x, y):  # Write calculateProduct function here
+    a = x * y
+    print(a)
+
 
 value1 = int(input("Enter first numeric value: "))
 value2 = int(input("Enter second numeric value: "))
 
 # Call calculateSum
-
+calculateSum(value1, value2)
 # Call calculateDifference
-
+calculateDifference(value1, value2)
 # Call calculateProduct
+calculateProduct(value1, value2)
