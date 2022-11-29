c1621a5ed563344fd1d31e67696b3574b0bec806
Towskister <Towskister@gmail.com>
2022-11-29 01:07:55 -0500

    added Final

---
+++ Final Programming Project Dice Game/RgutowskiDiceGame.py
@@ -0,0 +1,12 @@
+
+
+
+'''
+https://codegolf.stackexchange.com/questions/2602/draw-dice-results-in-ascii
+
+from random import*
+r=randrange(6)
+C='o '
+s='-----\n|'+C[r<1]+' '+C[r<3]+'|\n|'+C[r<5]
+print s+C[r&1]+s[::-1]
+'''
