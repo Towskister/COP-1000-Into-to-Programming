7c7d04cf53dd4d0cfab6c743943a51a20fa18f6a
Towskister <Towskister@gmail.com>
2022-11-28 22:32:47 -0500

    completed func sing prams

--- Writing Functions that Require a Single Parameter in Python/SumAndProduct.py
+++ Writing Functions that Require a Single Parameter in Python/SumAndProduct.py
@@ -6,18 +6,31 @@ Output:  Computed sum and product.
 
 
 # Write sums() function here.
+def sums(y):
+    x = 2
+    a = 1
+    while x != y+1:
+        a = a + x
+        x += 1
+    print(a)
 
 
 # Write products() function here.
-
+def products(y):
+    x = 2
+    a = 1
+    while x != y+1:
+        a = a * x
+        x += 1
+    print(a)
 
 numberString = input("Enter a positive integer or 0 to quit: ")
 number = int(numberString)
 
 while number != 0:
-    # Call sums() function here.
+    sums(number)  # Call sums() function here.
 
-    # Call products() function here.
+    products(number)  # Call products() function here.
 
     numberString = input("Enter a positive integer or 0 to quit: ")
     number = int(numberString)
