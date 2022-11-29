72d1a69aff889ea0f5a6fd24d66c6dc42fa5282f
Towskister <Towskister@gmail.com>
2022-11-28 22:02:47 -0500

    Added func sing prams

---
+++ Writing Functions that Require a Single Parameter in Python/SumAndProduct.py
@@ -0,0 +1,23 @@
+"""
+SumAndProduct.py - This program computes sums and products.
+Input:  Interactive.
+Output:  Computed sum and product.
+"""
+
+
+# Write sums() function here.
+
+
+# Write products() function here.
+
+
+numberString = input("Enter a positive integer or 0 to quit: ")
+number = int(numberString)
+
+while number != 0:
+    # Call sums() function here.
+
+    # Call products() function here.
+
+    numberString = input("Enter a positive integer or 0 to quit: ")
+    number = int(numberString)
