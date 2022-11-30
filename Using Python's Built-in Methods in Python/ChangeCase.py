4f38babd2799496c89044b16a06b90136dc0533c
Towskister <Towskister@gmail.com>
2022-11-30 17:15:48 -0500

    added Using Python's Built-in Methods in Python.iml

---
+++ Using Python's Built-in Methods in Python/ChangeCase.py
@@ -0,0 +1,16 @@
+"""
+ChangeCase.py - This program converts a string to lowercase and uppercase.
+Input:  Interactive
+Output:  Uppercase and lowercase versions of the user-entered string.
+"""
+
+sample = input("Enter a string or done when you want to quit: ")
+
+while sample != "done":
+    # Call the lower method here and print the result.
+
+    print("Lowercase: " + result)
+    # Call upper method here and print the result.
+
+    print("Uppercase: " + result)
+    sample = input("Enter a string or done when you want to quit: ")
